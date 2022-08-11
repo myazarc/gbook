b4cfbc124e50c9d8786588aec80c3ecf26aff980
myazarc <myazarc@yandex.com>
2022-08-11 13:46:58 +0300

    connected microservice

--- book-service/src/routes/index.ts
+++ book-service/src/routes/index.ts
@@ -2,12 +2,17 @@ import { Router } from "express";
 import { BookController } from "../controllers/book.controller";
 import { BookmarkController } from "../controllers/bookmark.controller";
 import { auth } from "../middlewares/jwt.middleware";
+import proxy from "express-http-proxy";
+
+const proxyBaseUrl = `http://${process.env.USER_SERVICE_HOST}:${process.env.USER_SERVICE_PORT}`;
 
 const publicRouter = Router();
 const bookController = new BookController();
 const bookmarkController = new BookmarkController();
 
 publicRouter.get("/search", bookController.search);
+publicRouter.get("/login", proxy(proxyBaseUrl + "/login"));
+publicRouter.get("/register", proxy(proxyBaseUrl + "/register"));
 
 const protectedRouter = Router();
 
